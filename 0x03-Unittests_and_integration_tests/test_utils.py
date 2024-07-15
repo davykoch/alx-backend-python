f6dc4d6255529a06a2643cfdc94ffc3e5596a12f
davykoch <koechdavis24@gmail.com>
2024-07-16 00:46:19 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,10 +25,29 @@ class TestAccessNestedMap(unittest.TestCase):
             expected: any
     ) -> None:
         """
-        Test that access_nested_map returns
-        the expected result for given inputs.
+        Test that access_nested_map returns the expected
+        result for given inputs.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: dict,
+            path: tuple,
+            expected_exception_msg: str
+    ) -> None:
+        """
+        Test that access_nested_map raises KeyError with correct message
+        for invalid inputs.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), f"'{expected_exception_msg}'")
 
 
 if __name__ == '__main__':
